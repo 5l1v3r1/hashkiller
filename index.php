85f202bfa9cc3dfe3a3b4ab342ad41e8eac72ea2
Ardhana <reyardn@gmail.com>
2019-04-28 21:17:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,11 @@ require_once "vendor/autoload.php";
 use \Hashkiller\MD5\Unhash;
 
 $hashkiller = new Unhash;
+
 header("Content-type: Application/Json");
+header("Service-API: Hashkiller");
+header("Version: 1.0 (Beta)");
+header("Author: Ardhana <zeebploit212@gmail.com>");
 
 if (isset($_GET["md5"])) {
     echo $hashkiller->crack($_GET["md5"])->json($hashkiller->getResult())->get();
